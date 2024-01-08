4a1ebef7cf623f9f1dca2ab25981317cb37d8a8e
SynergiseIT61 <155957259+SynergiseIT61@users.noreply.github.com>
2024-01-08 21:14:26 +1100

    Add files via upload

---
+++ CreateOneDriveSyncCSV.ps1
@@ -0,0 +1,20 @@
+﻿try {
+    Unblock-File -Path C:\ODTool\OneDriveLib.dll
+    Import-Module C:\ODTool\OneDriveLib.dll
+
+    $onedriveProcess = Get-Process -Name OneDrive -ErrorAction SilentlyContinue
+
+    if ($onedriveProcess) {
+        $Results = Get-ODStatus
+        $ResultsOutput = $Results | Select LocalPath, StatusString
+        $ResultsOutput | Export-CSV C:\ODTool\SyncStatus.csv -NoTypeInformation
+    } else {
+        $ResultsOutput = New-Object PSObject -Property @{
+            Message = "OneDrive is not running."
+        }
+        $ResultsOutput | Export-CSV C:\ODTool\SyncStatus.csv -NoTypeInformation
+    }
+
+} catch {
+    $_.Exception.Message | Out-File -FilePath C:\ODTool\ErrorLog.txt -Append
+}
